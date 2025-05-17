72d895fba53f6a290ae2661690d1156a4e392662
Gabi Ferraz <ferrazmgabi@gmail.com>
2025-05-17 15:43:08 -0300

    starting project

---
+++ src/test/java/com/api/orderReceived/OrderReceivedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.orderReceived;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderReceivedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
